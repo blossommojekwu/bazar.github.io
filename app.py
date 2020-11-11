b34f021be8bd572ec4e57a4f44ec62809aff57ad
Ruopu Jiao <rj117@duke.edu>
2020-11-11 04:14:14 -0500

    PLEASE READ! I added a base html file that contains the dropdown and conditional linking, when you make your html files and want the dropdown please just extend like I've done off the base, Also added login functionality, along with seller tables, along with seller/buyer distinction

--- app.py
+++ app.py
@@ -1,29 +1,115 @@
-from flask import Flask, render_template, request, redirect      
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from datetime import timedelta 
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import yaml
+from flask_sqlalchemy import sqlalchemy
 
+
+# Substantiate flaskapp
 app = Flask(__name__)
 
+# Make secret key for session data
+app.secret_key = "yeet"
+
 # Configure db
+# CHECK DB.YAML FILE TO SEE IF ALL OF THE PARAMETERS MATCH ON YOUR LOCAL MACHINE
 db = yaml.load(open('./templates/db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_PASSWORD'] = db['mysql_password'] #THIS WILL BE DIFFERENT, CHECK YAML FILE AND CHANGE ACCORDINGLY, ALSO DON'T PUSH THE YAML
 app.config['MYSQL_DB'] = db['mysql_db']
-
 mysql = MySQL(app)
 
+# Home page, renders homepage.html
 @app.route("/")
 def home():
-    return render_template("homepage.html")
+    if "user" in session:
+        logvar = True 
+        first_name = session["first_name"]
+        return render_template("homepage.html", logvar = logvar, first_name = first_name)
+    else:
+        logvar = False
+    return render_template("homepage.html", logvar = logvar)
 
-@app.route("/login")
+
+
+
+
+# Login page, renders login.html and gets session values for
+# firstname
+# user (email)
+# password
+# seller (boolean)
+@app.route("/login", methods = ["POST","GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST": # If the method that is called in login.html is a post method
+        # Store Values from the form into user and password variables
+        user = request.form["nm"]
+        password = request.form["pw"]
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) # This opens a cursor that can interact with the databases
+        cursor.execute('SELECT * FROM buyers WHERE email = %s AND password = %s',(user,password)) # Selects all buyers where email and password both match
+        account = cursor.fetchone() # takes one of these instances into account
+        if account: # If succesfully found in database
+            cursor =  mysql.connection.cursor() #Opens another Cursor
+            cursor.execute('SELECT first_name FROM buyers WHERE email = %s AND password = %s',(user,password))
+            first_name = cursor.fetchone()
+            cursor.execute('SELECT userID FROM buyers WHERE email = %s AND password = %s',(user,password))
+            userID = cursor.fetchone()
+            #Give email (user), password, first_name, userID variables to the session 
+            session["first_name"] = first_name[0]
+            session["userID"] = userID[0]
+            session["user"] = user
+            session["password"] = password
+            # Check seller table to see if buyer/user is also a seller
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM sellers WHERE email = %s', [user])
+            seller = cursor.fetchone()
+            # Set session seller status to corresponding boolean 
+            if seller:
+                session["seller"] = True
+            else:
+                session["seller"] = False
+            flash("Login Succesful!") # Flash a message that says login succesful 
+            return redirect(url_for("home")) # Redirects to home page
+        else: # If login is unsuccesful
+            # Redirects to login page and flashes a message that login was incorrect
+            flash("Incorrect Login!") 
+            return redirect(url_for("login"))
+    # If no method call 
+    else:
+        # if already logged in 
+        if "user" in session:
+            flash("Already Logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
 
-@app.route("/user")
+@app.route("/forgotpw")
+def forgotpw():
+    return "<h1>Sorry dude tough luck</h1>"
+
+# Logout page, clears session
+@app.route("/logout")
+def logout():
+    # notifies that you've been logged out
+    flash("You have been logged out", "info") #warning, info, error
+    # Pops the user info out of the session
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("home"))
+
+
+@app.route("/user", methods = ["POST", "GET"])
 def user():
-    return render_template("user.html")
+    if "user" in session:
+        logvar = True
+        first_name = session["first_name"]
+        seller = session["seller"] # Only works with Apu for current database
+        return render_template("user.html", logvar = logvar, first_name = first_name, seller = seller)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
 
 @app.route("/registration")
 def registration():
@@ -45,9 +131,20 @@ def item():
 def addreview():
     return render_template("addreview.html")
 
-@app.route("/seller")
+@app.route("/seller", methods = ["POST","GET"])
 def seller():
-    return render_template("seller.html")
+    if "user" in session and session["seller"] == True:
+        logvar = True
+        first_name = session["first_name"]
+        sellerID = session["userID"]
+        seller = session["seller"]
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT name, price, num, image FROM items WHERE sellerID = %s', [sellerID])
+        items = cursor.fetchall()
+        return render_template("seller.html", logvar = logvar, first_name = first_name, seller = seller, items = items)
+    else:
+        flash("You are not logged in/a seller")
+        return redirect(url_for("home"))
 
 @app.route("/addbalance")
 def addbalance():
@@ -56,15 +153,6 @@ def addbalance():
 @app.route("/purchasehistory")
 def purchasehistory():
     return render_template("purchasehistory.html")
-
-@app.route("/tradehistory")
-def tradehistory():
-    return render_template("tradehistory.html")
-
-@app.route("/sellinglist")
-def sellinglist():
-    return render_template("sellinglist.html")
-
 
 # @app.route("/account/seller", methods =['GET', 'POST'])
 # def seller():
