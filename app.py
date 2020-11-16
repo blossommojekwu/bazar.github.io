36e5d7d2d917218766fbd787b0724944e7e7e462
Avanti Shah <ars102@duke/edu>
2020-11-17 03:34:12 +0530

    changes to add review

--- app.py
+++ app.py
@@ -236,19 +236,15 @@ def addreview():
        userID = session["userID"]
        # Open a cursor and get current balance for user
        cursor = mysql.connection.cursor()
+       input_itemID = request.form["itemid"]
        input_stars = request.form["stars"]
        input_comments = request.form["body"]
-       cursor.execute('INSERT INTO ItemReview VALUES(userID, itemID, input_stars, input_comments')
+       cursor.execute('INSERT INTO ItemReview VALUES(userID, input_itemID, input_stars, input_comments')
        myReview = cursor.fetchone()
        return render_template("addreview.html", logvar = logvar, userID = userID, first_name = first_name, last_name = last_name, myReview = myReview)
-   else: # If you somehow accessed this page and weren't logged in
-       flash("You are not logged in to add a revoew")
+    else: # If you somehow accessed this page and weren't logged in
+       flash("You are not logged in to add a review!")
        return redirect(url_for("home"))
-
-    (buyerID INTEGER NOT NULL REFERENCES Buyers(userID), -- Field set by website
-itemID INTEGER NOT NULL REFERENCES Items(itemID), -- Field set by website
-numStars INTEGER NOT NULL, -- Field for User to input
-comments VARCHAR(1250), -- Optional, field for User to input
 
 @app.route("/seller", methods = ["POST","GET"])
 def seller():
